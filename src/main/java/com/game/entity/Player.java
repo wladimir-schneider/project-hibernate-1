4111d8715db2ecc169d76591d6e07403946d609c
Wladimir <wladimirs989@gmail.com>
2026-04-02 08:33:12 +0200

    Project completed

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,45 @@
 package com.game.entity;
 
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
+
+//import javax.persistence.*;
+
 import java.util.Date;
 
-
+@Entity
+@Table(name = "player", schema = "rpg")
+@NamedQuery(name = "player_getAllCount", query = "select count(p) from Player p")
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
 
+    @Column(length = 12, nullable = false)
     private String name;
 
+    @Column(length = 30, nullable = false)
     private String title;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
     private Race race;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
     private Profession profession;
 
+    @Column(nullable = false)
     private Date birthday;
 
+    @Column(nullable = false)
     private Boolean banned;
 
+    @Column(nullable = false)
     private Integer level;
 
     public Player() {
--- src/main/java/com/game/repository/IPlayerRepository.java
+++ src/main/java/com/game/repository/IPlayerRepository.java
@@ -1,6 +1,7 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.HibernateException;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,7 +9,7 @@ import java.util.Optional;
 public interface IPlayerRepository {
     List<Player> getAll(int pageNumber, int pageSize);
 
-    int getAllCount();
+    int getAllCount() throws HibernateException;
 
     Player save(Player player);
 
